785ca267c64f99a3c1a0b4f91c7252c30470837c
developer2 <mazzachris.work+developer2@gmail.com>
2022-04-02 11:06:29 -0500

    Added initial spring-boot files generated from Spring Initializr

---
+++ src/main/java/mine/mazzanet/shinypancake/ShinyPancakeApplication.java
@@ -0,0 +1,13 @@
+package mine.mazzanet.shinypancake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinyPancakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShinyPancakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mine/mazzanet/shinypancake/ShinyPancakeApplicationTests.java
@@ -0,0 +1,13 @@
+package mine.mazzanet.shinypancake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinyPancakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
